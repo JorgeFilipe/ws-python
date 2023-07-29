c2d572b3d80db76f05a8b077839c565631ac9519
Jorge Macedo <jorgefilipedasilva@gmail.com>
2023-07-29 18:08:32 -0300

    changes

---
+++ IFRJ/list-A/meiaColaSoda.py
@@ -0,0 +1,14 @@
+# The Meia-Cola soft drink factory sells products in 3 formats:
+# Can (350ml) ;
+# bottle (600ml) ;
+# bottle (2 liter)
+#
+# Calculate how many liters of soda will be purchased
+
+def convertToLiters(value):
+    liter = (value/1000)
+    return liter
+
+can = 350 # in mL
+bottle1 = 600 # in mL
+bottle2 = 2000 # in mL
