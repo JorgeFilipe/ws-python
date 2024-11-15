beef97d3f2156a8f883db6b5686d676a885939cc
Jorge Macedo <jorgefilipedasilva@gmail.com>
2024-11-15 18:30:16 -0300

    .

--- maratona-python/aula7_Control_Flow.py
+++ maratona-python/aula7_Control_Flow.py
@@ -6,17 +6,40 @@ def unlock_adult_movies(username, age):
     return msg
 
 print("==================================================")
-print()
+print() ## QUEBRA DE LINHA
 print(unlock_adult_movies("John", 12))
-print()
+print() ## QUEBRA DE LINHA
 print(unlock_adult_movies("Steve", 18))
-print()
+print() ## QUEBRA DE LINHA
 print(unlock_adult_movies("Bob", 25))
-print()
+print() ## QUEBRA DE LINHA
 print(unlock_adult_movies("James", 10))
-print()
+print() ## QUEBRA DE LINHA
 print(unlock_adult_movies("Alexssander", 11))
-print()
+print() ## QUEBRA DE LINHA
 print(unlock_adult_movies("Dylan", 13))
-print()
+print() ## QUEBRA DE LINHA
 print("==================================================")
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+print() ## QUEBRA DE LINHA
+username=input("Nome de Usuário:")
+senha=input("Senha:")
+
+## INFORMAÇÔES ARMAZENADAS DENTRO DO BANCO DE DADOS ##
+senhaStoredBD="123abc"
+usuarioStoredBD="admin"
+#####################################################
+
+def verificar_senha(username, senha):
+    if username != usuarioStoredBD and senha != senhaStoaredBD:
+        msg="Falha no Login"
+    else:
+        msg="Login com Sucesso"
+    return msg
+
+
+print() ## QUEBRA DE LINHA
+print(verificar_senha(username, senha))
+print() ## QUEBRA DE LINHA
