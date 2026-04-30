3aee3aa7562246c4f3131cb2145b9452833bc930
Jorge Macedo <jorgefilipedasilva@gmail.com>
2026-04-30 09:36:53 -0300

    Update aula11_Requests.py

--- Courses/maratona-python/aula11_Requests.py
+++ Courses/maratona-python/aula11_Requests.py
@@ -4,6 +4,9 @@
 
 import requests
 
+print#LINEBREAKER💣💥
+print#LINEBREAKER💣💥
+
 ##url_catho = "https://www.catho.com.br/vagas/python/"
 url_indeed = "https://br.indeed.com/jobs?q=python"
 ##request2 = requests.get(url_catho)
@@ -20,17 +23,40 @@ if r.status_code == 200:
 else:
     print("Site Offline")
 
-print#LINEBREAKER💣💥
-print#LINEBREAKER💣💥
-
-#¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_
+print()#LINEBREAKER💣💥
+print("#¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#")
+print()#LINEBREAKER💣💥
 
 url_teste_status_200=requests.get("https://icanhazip.com/") ## O IcanhazIP retorna o IP Público.
 print()
 print("URL Acessada", url_teste_status_200)
 print("Status HTTP:", url_teste_status_200.status_code)
 print("TEXTO DE RESPOSTA:")
-print#LINEBREAKER💣💥
+print()#LINEBREAKER💣💥
 print(url_teste_status_200.text)
 
+print()#LINEBREAKER💣💥
+print("#¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#")
+print()#LINEBREAKER💣💥
 
+print("Informe o assundo a ser pesquisado:")
+assunto=input("🔎: ")
+google=requests.get(f"https://www.google.com/search?q={assunto}")
+print("Status HTTP:", google.status_code) ##
+print("Conteúdo da Página:") ##
+print()##Linebreaker
+print("-------------------------------------------------------------------------------------") ##
+#print(google.text) ##
+print("_____________________________________________________________________________________") ##
+
+print()#LINEBREAKER💣💥
+print("#¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#_#¨#_¨#")
+print()#LINEBREAKER💣💥
+
+sport_result=requests.get("https://www.flashscore.com.br/equipe/flamengo/WjxY29qB/resultados/")
+print("Status HTTP:", sport_result.status_code) ##
+print("Conteúdo da Página:") ##
+print()##Linebreaker
+print("-------------------------------------------------------------------------------------") ##
+#print(sport_result.text) ##
+print("_____________________________________________________________________________________") ##
