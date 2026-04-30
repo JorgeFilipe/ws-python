0fc16e1bb0a598e932b9b799c5a60a9576f0f7d0
Jorge Macedo <jorgefilipedasilva@gmail.com>
2026-04-30 16:51:26 -0300

    Update desafio_4.py

--- Courses/maratona-python/desafio_4.py
+++ Courses/maratona-python/desafio_4.py
@@ -1,7 +1,8 @@
 import requests
-import DES4HTTPStatusCodeLib
+import httpstatuscodelib # módulo criado para mapear os status codes HTTP com seus significados.
 
+url_indeed = requests.get("https://br.indeed.com/jobs?q=python")
 r=requests.get("https://www.google.com/") ## O Google é um site que tem alta disponibilidade, ou seja, dificilmente fica offline.
 print("Status HTTP:", r.status_code) ## O status code 200 é o código de
-print(status_code_lib[r.status_code]) ## O status code 200 é o código de "OK", ou seja, a requisição foi bem sucedida.
+print(httpstatuscodelib.status_code_meaning(r.status_code)) ## O status code 200 é o código de "OK", ou seja, a requisição foi bem sucedida.
 
