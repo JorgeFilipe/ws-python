f59333606777136f45413edca2a7f613880d19cb
Jorge Macedo <jorgefilipedasilva@gmail.com>
2026-05-01 17:24:25 -0300

    .

--- Examples/Pesquisando_strings_em_Lists.py
+++ Examples/Pesquisando_strings_em_Lists.py
@@ -1,3 +1,5 @@
+import requests
+import httpstatuscodelib
 
 list_urls=['google', 'icanhazip.com', 'ip-api', 'youtube.com']
 for i in list_urls:
@@ -20,4 +22,29 @@ for w in list_words:
         print()#QUEBRALINHA
         print(w," é uma palavra que NÃO contém \"as\" ❌")
 
+
+print()#QUEBRALINHA
+print("-------------------------------------------")#QUEBRALINHA
+print()#QUEBRALINHA
+
+# PESQUISANDO SE AS URLS POSSUEM PONTO "." E SE SÃO VÁLIDAS E DEPOIS SEPARANDO EM LISTAS DIFERENTES.
+urls_invalidas=[]
+urls_validas=[]
+urls=['google.com', 'https://ip-api.com', 'dd2d2wed233423', 'icanhazip.com', 'Wasd18dw1', 'janela', 'https://bitly.com/']
+
+for url in urls:
+    if "." in url in urls: # PESQUISA SE DENTRO DO ITEM POSSUI "as".
+        urls_validas.append(url)
+    else:
+        urls_invalidas.append(url)
+
+# NOVA LISTA = urls_validas + urls_inválidas
+
+
+print()#QUEBRALINHA
+print("URLs Válidas ✅:")
+print(urls_validas)
+print()#QUEBRALINHA
+print("URLs Inválidas ❌:")
+print(urls_invalidas)
 print()#QUEBRALINHA
