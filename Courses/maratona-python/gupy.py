d0ff3be319bf8b84d19c4b3488f48786d7014875
Jorge Macedo <jorgefilipedasilva@gmail.com>
2026-06-13 22:53:55 -0300

    Update gupy.py

--- Courses/maratona-python/gupy.py
+++ Courses/maratona-python/gupy.py
@@ -8,33 +8,26 @@ api_gupy="https://employability-portal.gupy.io/api/v1/jobs?jobName=python&limit=
 
 r_gupy = requests.get(api_gupy)
 
-html_gupy=r_gupy.text
-print(html_gupy[:5000])
+dados = r_gupy.json()
 
-soup=BeautifulSoup(html_gupy,'html.parser')
+#print(type(dados))
+jobs=[]
+for dado in dados['data']:
+    job = {
+        'title': dado['name'], ## CAPTURA O TITLE
+        'company': dado['careerPageName'], ## CAPTURA A COMPANY
+        'location': dado['city']+" - "+dado['state']+" - "+dado['country'],
+        'how_old':  dado['publishedDate'] ## CAPTURA A IDADE DA VAGA
+    }
+    jobs.append(job)
 
-cards = soup.find_all('div', class_="sc-fjvvzt tyvCs sc-3b0b8240-5 FPfdd") # procurar DIV com CLASS contais = sc-cwHptR cgrzzg sc-3b0b8240-2 kUpsCU
+print(jobs)
 
-#print(soup.prettify())
-#print(soup.find('div', class_="sc-fjvvzt tyvCs sc-3b0b8240-5 FPfdd"))
-
-#jobs=[]
-#for card in cards:
-#    ##-------CAPTURANDO A EMPRESA---------------
-#    company_class = card.find('a', class_="text-body text-decoration-none") ## SALVA A TAG INTEIRA Q CONTÉM A COMPANY
-#    if company_class:
-#        company = company_class.get_text(" ", strip=True) ## Pega a EMPRESA e formata removendo espaços
-#    else:
-#        company="Empresa Confidencial"
-#    ##------CAPTURANDO O LOCATION---------------
-#    location_raw = card.find('div', class_="mb-8").get_text() ## CAPTURA O LOCATION
-#    split_location = [item.strip() for item in location_raw.split(',')]
-#    location = split_location[0]
-#    job = {
-#        'title': card.find('h2', class_="js_vacancyTitle").string.lstrip().rstrip(), ## CAPTURA O TITLE
-#        'company': company,
-#        'location': location,
-#        'how_old': card.find('div', class_="text-medium small text-nowrap").get_text().lstrip().rstrip() ## CAPTURA A IDADE DA VAGA
-#    }
-#    jobs.append(job)
-#
+for job in jobs:
+    print(job['title'])
+    print(job['company'])
+    print(job['location'])
+    print(job['how_old'])
+    print()
+    print("-----------------------------")
+    print()
